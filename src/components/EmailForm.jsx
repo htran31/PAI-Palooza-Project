9332a0b6853c9ceb3de9496bf1c02a3d14be409a
Brendan Sick <brendansick@gmail.com>
2024-08-04 01:59:29 -0700

    Revert

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -7,7 +7,7 @@ const EmailForm = () => {
   const handleEmailSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('https://mighty-island-16463-cdb9931bccaa.herokuapp.com/', {
+      const response = await fetch('http://localhost:3001', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
